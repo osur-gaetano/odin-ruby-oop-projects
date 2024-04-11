839a71b0ac3606633f6af82f37c8a84aa687e9a6
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-04-11 14:51:30 +0300

    Add board methods to class Board

--- Tic-Tac-Toe/board.rb
+++ Tic-Tac-Toe/board.rb
@@ -9,33 +9,46 @@ class Board
   end
 
   def to_s
-    " | |   |   |
-    __| #{board_cells[0]}  | #{board_cells[1]} | #{board_cells[2]} |__
-    __| #{board_cells[3]}  | #{board_cells[4]} | #{board_cells[5]} |__
-    __| #{board_cells[6]}  | #{board_cells[7]} | #{board_cells[8]} |__
-      | |   |   |"
+    "      |___|___|___|
+    __| #{board_cells[0]}  | #{board_cells[1]}  |  #{board_cells[2]} |__
+    __| #{board_cells[3]}  | #{board_cells[4]}  |  #{board_cells[5]} |__
+    __| #{board_cells[6]}  | #{board_cells[7]}  |  #{board_cells[8]} |__
+      |   |   |   |"
   end
 
   def change_position(player)
-    if board_cells.any?(nil)
-      if board_cells[player.position - 1].nil?
-        board_cells[player.position - 1] = player.marker
+    if board_cell_empty?
+      if check_nil_cell?(player)
+        board_cells[player.position] = player.marker
       else
         change_position(player)
       end
     else
-      'The board is already full'
+      puts 'The board is already full'
+    end
+  end
+
+  def board_cell_empty?
+    board_cells.any? nil
+  end
+
+  def check_nil_cell?(player)
+    if board_cells[player.position].nil?
+      true
+    else
+      false
     end
   end
 
   def win_check(player, opponent)
     if cell_check(player)
-      "#{player.name} wins the game"
-
+      puts "#{player.name} wins the game"
     elsif cell_check(opponent)
-      "#{opponent.name} wins the game"
-    else
-      'This is a draw'
+      puts "#{opponent.name} wins the game"
+    elsif board_cell_empty?
+      puts 'The board still hase empty cells, continue playing'
+    elsif !board_cell_empty?
+      puts 'The board is full, this is a draw'
     end
   end
 
@@ -52,5 +65,9 @@ class Board
     else
       false
     end
+  end
+
+  def clean_the_board
+    self.board_cells = Array.new(9, nil)
   end
 end
